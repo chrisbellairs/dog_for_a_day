21bc12079400fdfe3b45777c9f7ccd4e4d54a509
Freddie Fryer <freddiefryercode@gmail.com>
2021-02-09 20:21:20 +0000

    added new attributes to Users table

---
+++ db/migrate/20210209201839_add_user_attributes_to_users.rb
@@ -0,0 +1,8 @@
+class AddUserAttributesToUsers < ActiveRecord::Migration[6.0]
+  def change
+    add_column :users, :first_name, :string
+    add_column :users, :last_name, :string
+    add_column :users, :address, :text
+    add_column :users, :postcode, :string
+  end
+end
