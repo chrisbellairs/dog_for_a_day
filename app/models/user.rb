c20b57962c07b1d4c8329aa287aa9845b69e6d2e
Matt Woods <woodsmj17@gmail.com>
2021-02-10 22:11:44 +0000

    added views to dog, tweaked validations to match sliders in view

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,5 +8,5 @@ class User < ApplicationRecord
   has_many :bookings, dependent: :destroy
 
   validates :first_name, :last_name, :address, presence: true
-  validates :postcode, format: { with: /^([A-Z][A-HJ-Y]?[0-9][A-Z0-9]? ?[0-9][A-Z]{2}|GIR ?0A{2})$/ }
+  validates :postcode, format: { with: /\A([A-Z][A-HJ-Y]?[0-9][A-Z0-9]? ?[0-9][A-Z]{2}|GIR ?0A{2}\z)/ }
 end
