2ed07afa2796ab3b2c48a2d3d007d6d8bb7c1226
Matt Woods <woodsmj17@gmail.com>
2021-02-10 19:03:37 +0000

    added validations and associations, but still need to test with front end

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -1,4 +1,7 @@
 class Dog < ApplicationRecord
   belongs_to :user
   has_many :bookings
+
+  validates :name, uniqueness: { scope: :user }
+  validates :biography, :breed, :age, :size, :good_with_children, :walk, :energy, :friendly, presence: true
 end
