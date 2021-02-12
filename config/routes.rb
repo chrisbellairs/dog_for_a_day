43c6fc11cc4c82b08bd3338624bf93f9a9b558ee
Matt Woods <woodsmj17@gmail.com>
2021-02-12 17:44:33 +0000

    modal works to log user in, but error messages do not display

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
     resources :bookings, except: [:index]
   end
 
-  devise_for :users
+  devise_for :users, controllers: {
+        sessions: 'users/sessions'
+}
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
