57ae6a15ddee32b9e355bad446c5fb61b89bb075
Chris Bellairs <c.m.bellairs@gmail.com>
2021-02-18 22:12:30 +0000

    Created index card and class methods for statistics displayed on card.

--- app/models/dog.rb
+++ app/models/dog.rb
@@ -10,4 +10,25 @@ class Dog < ApplicationRecord
   validates :name, uniqueness: { scope: :user }
   validates :biography, :breed, :age, :good_with_children, presence: true
   validates :size, :walk, :energy, :friendly, presence: true, inclusion: { in: (1..10) }, numericality: { only_integer: true }
+
+  def walk_length
+    if self.walk < 4
+      "Short walks"
+    elsif self.walk < 7
+      "Average walks"
+    else
+      "Long walks"
+    end
+  end
+
+  def dog_size
+    if self.size < 4
+      "Small dog"
+    elsif self.size < 7
+      "Medium god"
+    else
+      "Large dog"
+    end
+  end
+
 end
